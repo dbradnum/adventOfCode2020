d5b8981c87540ce1a29cf0ea0cbd128b15cf0c44
David Bradnum <dbradnum@gmail.com>
2020-12-07 21:36:47 +0000

    second half of day 7, incl a bit of refactoring to the first half - create the graph with weights, and with edges running in a more intuitive direction.

--- code/day7.R
+++ code/day7.R
@@ -12,18 +12,42 @@ relationships = raw %>%
 
 links = relationships %>% 
   filter(inner != "no other") %>% 
-  separate(inner," ",into = c("noOfBags","inner"),extra = "merge")
+  separate(inner," ",into = c("weight","inner"),extra = "merge") %>% 
+  mutate(weight = as.integer(weight))
 
 
-g = graph_from_data_frame(links %>% select(inner,outer))
+g = graph_from_data_frame(links %>% select(outer,inner,weight))
 
-# plot(g)
+# plot.igraph(g,
+#             edge.label = E(g)$weight)
 
-connected = ego(g,length(V(g)), "shiny gold",mode = "out")
+# find all the vertices that have a path "shiny gold"
+upstream = unlist(ego(g,length(V(g)), "shiny gold",mode = "in"))
 
-length(connected[[1]]) - 1
-
+length(upstream) - 1
 
 # part 2 ------------------------------------------------------------------
 
+# find all the vertices that can be reached from "shiny gold" - ie live inside that bag
+downstream = unlist(ego(g,length(V(g)), "shiny gold",mode = "out"))
 
+# Find the number of bags implied by a specific path (seq of vertices), by multiplying edge weights.
+# This doesn't feel optimal - surely there must be a better way to get edge weights along a path?!
+getWeightForPath = function(path){
+  weight = 1
+  for(i in 1:(length(path) - 1)){
+    eid = get.edge.ids(g,c(path[i],path[i+1]))
+    weight = weight * E(g)[eid]$weight 
+  }
+  weight
+}
+
+# Find the number of bags implied by all paths to a particular node (ie colour)
+getAllWeightsForNode = function(nodeId){
+  paths = all_simple_paths(g,"shiny gold",nodeId,mode = "out")
+  
+  sum(map_dbl(paths,getWeightForPath))
+}
+
+# and finally sum up the totals for each downstream node
+sum(map_dbl(downstream,getAllWeightsForNode))
