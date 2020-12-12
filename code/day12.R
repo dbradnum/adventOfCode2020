d5a494de2c3b0e99e091b3d13f5734cb4e970448
David Bradnum <dbradnum@gmail.com>
2020-12-12 12:34:26 +0000

    completed day 12; amended first half to use vector notation (as I should've done in the first place!!)

--- code/day12.R
+++ code/day12.R
@@ -1,19 +1,19 @@
-library(sets)
 library(tidyverse)
+library(expm)
 
 instructions = read_csv("inputs/12.txt",col_names = "instruction") %>%
   separate(instruction,into = c("direction","qty"),sep = 1) %>% 
   mutate(qty = as.integer(qty))
 
-compass = list(tuple(1,0), # E
-               tuple(0,1), # S
-               tuple(-1,0), # W
-               tuple(0,-1)) # N
+compass = list(matrix(c(1,0)), # E
+               matrix(c(0,1)), # S
+               matrix( c(-1,0)), # W
+               matrix(c(0,-1))) # N
 
 directionMap = 1:4
 names(directionMap) = c("E","S","W","N")
 
-position = tuple(0,0)
+position = matrix(c(0,0))
 currentDirection = 1
 
 for (i in 1:nrow(instructions)) {
@@ -32,6 +32,42 @@ for (i in 1:nrow(instructions)) {
                           ifelse(iDirection == "L",-1,1) * (iQty/90) - 1) %% 4 + 1
   }
   # print(position)
+}
+
+print(str_glue("Sum of final coords = {abs(position[1]) + abs(position[2])}"))
+
+
+# part 2 ------------------------------------------------------------------
+
+position = matrix(c(0,0))
+wayPosition = matrix(c(10,-1))
+currentDirection = 1
+
+rotation = rbind(c(0,-1),
+                 c(1,0))
+
+for (i in 1:nrow(instructions)) {
+  # print(instructions[i,])
+  iDirection = instructions$direction[i]
+  iQty = instructions$qty[i]
+  
+  if (iDirection %in% names(directionMap)) {
+    wayPosition = wayPosition + compass[[directionMap[iDirection]]] * iQty
+    
+  } else if (iDirection == "F") {
+    position = position + wayPosition * iQty
+    
+  } else {
+    nRotations = (ifelse(iDirection == "L",-1,1) * (iQty/90) - 1) %% 4 + 1
+    
+    wayPosition = (rotation %^% nRotations) %*% matrix(c(wayPosition[[1]],
+                                                          wayPosition[[2]]),
+                                                          2,1)
+  }
+  # print("ship:")
+  # print(position)
+  # print("way:")
+  # print(wayPosition)
 }
 
 print(str_glue("Sum of final coords = {abs(position[1]) + abs(position[2])}"))
