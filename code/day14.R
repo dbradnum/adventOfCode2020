33a45512cd2419d6e03d21c41e39e232bc021dac
David Bradnum <dbradnum@gmail.com>
2020-12-14 23:30:19 +0000

    day 14 part 2... probably far from optimally!

--- code/day14.R
+++ code/day14.R
@@ -28,10 +28,15 @@ maskAndConvert = function(mask,baseValue){
   
   }
 
-getMaskCharacters = function(mask){
-  enframe(str_split(mask,pattern = "")[[1]],
-          name = "bit")  %>% 
-  filter(value != "X")
+getMaskCharacters = function(mask,excludeX = T){
+  bits = enframe(str_split(mask,pattern = "")[[1]],
+          name = "bit")  
+  
+  if (excludeX) {
+    bits = bits %>% 
+      filter(value != "X")
+  }
+  bits
 }
 
 mem = rep(NA,max(instructions$index,na.rm = T))
@@ -49,4 +54,63 @@ for (i in 1:nrow(instructions)){
 
 format(sum(mem,na.rm = T),scientific = F)
 
+# part 2 ------------------------------------------------------------------
+
+library(arrangements)
+
+getFloatingMaskPositions = function(mask,baseValue){
+  baseInBinary = dec2bin(baseValue)
+  
+  baseChars = str_split(baseInBinary,pattern = "")[[1]]
+  maskChars = str_split(mask,pattern = "")[[1]]
+  
+  overlaidChars = ifelse(maskChars == "0",baseChars,maskChars)
+  
+  nx = sum(overlaidChars == "X")
+  # print(nx)
+  
+  expandedXs = permutations(c(0,1),nx,replace = T)
+  
+  binarySequences = matrix(rep.int(overlaidChars,nrow(expandedXs)),
+                           nrow = nrow(expandedXs),
+                           byrow = T)
+  
+  colsToExpand = which(overlaidChars == "X")
+
+  binarySequences[,colsToExpand] = expandedXs
+
+  binarySequences = matrix(sapply(binarySequences,as.numeric),
+                           nrow = nrow(expandedXs))
+    
+  decimals = binarySequences %*% 2^(seq(35,0,by = -1))
+  decimals
+}
+
+
+results = tibble()
+for (i in 1:nrow(instructions)){
+  iOpType = instructions$opType[i]
+  iIndex = instructions$index[i]
+  iValue = instructions$value[i]
+  
+  if (iOpType == "mask") {
+    currentMask = iValue
+  } else {
+    positions = getFloatingMaskPositions(currentMask,as.integer(iIndex))
+    
+    iResult = tibble(index = i, position = as.vector(positions),value = as.integer(iValue))
+    results = results %>% bind_rows(iResult)
+  }
+}
+
+solution = results %>% #
+  arrange(index) %>% 
+  group_by(position) %>% 
+  top_n(1,wt = index) %>% 
+  ungroup() %>% 
+  summarise(solution = sum(value)) %>% 
+  pull(solution)
+  
+
+format(solution,scientific = F)
 
