2f256fffb6249b03458bda9ea9aa7d90cf92b4cc
David Bradnum <dbradnum@gmail.com>
2020-12-16 18:32:48 +0000

    day 16 part 2

--- code/day16.R
+++ code/day16.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-raw = read_lines("inputs/16example.txt")
+raw = read_lines("inputs/16.txt")
 
 ticketHeaderLines = which(str_detect(raw,"ticket"))
 
@@ -14,21 +14,77 @@ rules = raw[1:(ticketHeaderLines[1] - 2)] %>%
   mutate(across(c(min,max), as.integer)) %>% 
   select(fieldId,fieldName,min,max)
 
-myTicket = raw[ticketHeaderLines[1] + 1]
+myTicket = raw[ticketHeaderLines[1] + 1] %>% 
+  as_tibble() %>% 
+  mutate(fieldVal = str_split(value,pattern = ",")) %>% 
+  unnest_longer(fieldVal,indices_to = "position") %>% 
+  mutate(fieldVal = as.integer(fieldVal)) %>% 
+  select(position,fieldVal)
 
 nearbyTickets = raw[(ticketHeaderLines[2] + 1):length(raw)] %>% 
   as_tibble() %>% 
   mutate(fieldVal = str_split(value,pattern = ","),
          ticketId = row_number()) %>% 
-  unnest_longer(fieldVal) %>% 
+  unnest_longer(fieldVal,indices_to = "position") %>% 
   mutate(fieldVal = as.integer(fieldVal)) %>% 
-  select(ticketId,fieldVal)
+  select(ticketId,position,fieldVal)
 
-rules %>% 
+validation = rules %>% 
   crossing(nearbyTickets) %>% 
   mutate(inRange = (fieldVal >= min) & (fieldVal <= max)) %>% 
   group_by(ticketId,fieldVal) %>% 
   summarise(nValid = sum(inRange)) %>%
-  ungroup() %>% 
+  ungroup() 
+
+validation %>% 
   filter(nValid == 0) %>% 
   summarise(answer = sum(fieldVal))
+
+# part 2 ------------------------------------------------------------------
+
+validTickets = validation %>% 
+  group_by(ticketId) %>% 
+  summarise(allValid = (min(nValid) > 0)) %>% 
+  filter(allValid) %>%
+  select(ticketId) %>% 
+  inner_join(nearbyTickets)
+
+nTickets = validTickets %>% summarise(n_distinct(ticketId)) %>% pull()
+nFields = validTickets %>% summarise(n_distinct(position)) %>% pull()
+
+# Idea: find how many positions each ticket can slot into; we'll fill from least to most, and then recheck
+
+filledPositions = tibble(fieldId = numeric(),position = numeric() )
+while (nrow(filledPositions) < nFields){
+  
+  validPositions = rules %>% 
+    crossing(validTickets) %>% 
+    anti_join(filledPositions,by = "fieldId") %>% 
+    mutate(inRange = (fieldVal >= min) & (fieldVal <= max)) %>% 
+    group_by(fieldId,fieldName,position) %>% 
+    summarise(nValid = sum(inRange)) %>%
+    ungroup() %>% 
+    filter(nValid == nTickets)
+  
+  validOptionsPerPosition = validPositions %>% 
+    count(position) %>% 
+    arrange(n)
+  
+  nextSlot = validPositions %>% 
+    filter(position == validOptionsPerPosition %>% 
+             slice(1) %>% 
+             pull(position) )
+  
+  # print(nextSlot)
+  
+  # growing a dataframe like this is inefficient, but we only have 20 rows to deal with...
+  filledPositions = bind_rows(filledPositions,
+                              nextSlot %>% select(-nValid))  
+}
+
+myTicket %>% 
+  inner_join(filledPositions,by = "position") %>% 
+  filter(str_starts(fieldName,"departure")) %>% 
+  summarise(answer = prod(fieldVal))
+
+
